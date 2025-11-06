13e0557a9ef4f548c23619645e49520090e777f6
ldumlao1204 <leilany.dumlao@gmail.com>
2025-11-06 13:24:01 +0800

    edited bom.html and bom.js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,13 +2,41 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
 
 button.addEventListener('click', function () {
     // Code to execute when the button is clicked
+
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', 'Remove chapter');
+
+
+        li.append(deleteButton);
+
+        deleteButton.addEventListener('click', function () {
+
+            list.removeChild(li);
+            input.focus();
+
+        });
+
+
+        list.append(li);
+
+        input.value = '';
+
+    }
+
+    input.focus();
 });
+
+
+
+<button aria-label="Close" id="close-button">❌</button>
+
+
