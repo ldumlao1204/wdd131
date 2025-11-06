42aceec11425c3d9a6c7884887316bb9680e81ed
ldumlao1204 <leilany.dumlao@gmail.com>
2025-11-06 16:02:52 +0800

    updated debugging.html and debugging .js

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,8 +1,9 @@
+// get elements from the document
 const radiusOutput = document.getElementById('radius');
 const areaOutput = document.querySelector('area');
 
 let area = 0;
-const PI == 3.14159;
+const PI = 3.14159;
 
 const radius = 10;
 area = PI * radius * radius;
